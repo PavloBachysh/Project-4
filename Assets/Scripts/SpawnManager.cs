40fdf835c08b2959ec4e09543929e109736f491f
PavloBachysh <happytouni@gmail.com>
2025-03-31 01:27:14 +0300

    Project is done

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,12 +6,41 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
+    public GameObject powerup;
     private float spawnRange = 9.0f;
+    private int enemyCount;
+    private int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerUp();
+    }
+
+    private void Update()
+    {
+        enemyCount = enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerUp();
+        }
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private void SpawnPowerUp()
+    {
+        Instantiate(powerup, GenerateSpawnPos(), powerup.transform.rotation);
     }
 
     private Vector3 GenerateSpawnPos()
